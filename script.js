fb14fcb7b4eb0285c9583b9ba77fbcba2f4cb8fa
rishi21XAE <124399714+rishi21XAE@users.noreply.github.com>
2025-06-08 18:45:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/* script.js */
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Login submitted! (Functionality to be added)');
+});
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Registration submitted! (Functionality to be added)');
+});
